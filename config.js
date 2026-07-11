72ea614643dfd3f0db9b3f4ce3fde26fc6e5716a
rivailagc-stack <rivailagc@gmail.com>
2026-07-11 17:48:42 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = "https://qodtxydbzqmymsuvamza.supabase.co";
+const SUPABASE_KEY = "sb_publishable_l9HUHlKsyhyYhY9JWuIncA_EOuDEKeD";
+
+const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
